6d70d410edb1e148b574a7aa89b390dc5075fdaa
p9648213 <p9648213750912@gmail.com>
2025-02-17 07:31:09 +0700

    update trpc

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,13 +6,13 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
-export const user = pgTable(
+export const users = pgTable(
   "users",
   {
     id: uuid("id").primaryKey().defaultRandom(),
     clerkId: text("clerk_id").unique().notNull(),
     name: text("name").notNull(),
-    //TODO: add banner fields
+    // TODO: add banner fields
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
